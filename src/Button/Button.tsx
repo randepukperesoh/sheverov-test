43ae2adec7d9c9cc3132b808f8ae031abdd59acb
PC <mirgthebest@gmail.com>
2024-02-08 11:34:14 +0300

    finaly 3

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -4,8 +4,6 @@ import useThrottle from "../hooks"
 
 export const Button = React.memo(({onClick} :IButtonProps) : JSX.Element => { 
   const throttledCalback = useThrottle(() => onClick(), 300);
-  
-  console.log('memo')
     return  (
       <button type="button" onClick={throttledCalback}>
         get random user
