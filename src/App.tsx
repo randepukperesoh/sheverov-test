6947d4070f72a6c8402228d88c7b4fd6dac41af3
PC <mirgthebest@gmail.com>
2024-02-11 11:21:14 +0300

    added caching

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,27 @@ import {Button} from './Button/Button'
 const URL = "https://jsonplaceholder.typicode.com/users";
 
 function App(): JSX.Element {
-  const [item, setItem] = useState<User | null>(null);
+  const [item, setItem] = useState<Record<number , User> | undefined>(undefined);
+  const [idLastUser, setIdLastUser] = useState<number | undefined>(undefined);
 
   const receiveRandomUser = useCallback(async () => {
     const id = Math.floor(Math.random() * (10 - 1)) + 1;
+    setIdLastUser((prevItem) => id)
+    if (item && item[id]) {
+      return;
+    }
     const response = await fetch(`${URL}/${id}`);
     const _user = (await response.json()) as User;
-    setItem(_user);
-  },[]);
-  
+    setItem((prevItem) => {
+      return { ...prevItem, [id]: _user };
+    });
+  }, [item]);
+
   return (
     <div>
       <header>Get a random user</header>
       <Button onClick={receiveRandomUser} />
-      {item == null ? null : <UserInfo user={item} />}
+      {item && idLastUser && <UserInfo user={item[idLastUser]} />}
     </div>
   );
 }
--- src/UserInfo/UserInfo.tsx
+++ src/UserInfo/UserInfo.tsx
@@ -1,20 +1,25 @@
 import { IUserInfoProps } from "../types";
 
 export  function UserInfo({ user }: IUserInfoProps): JSX.Element { 
+  if (user === undefined) {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Phone number</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{user.name}</td>
-            <td>{user.phone}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div> 
+      Load
+    </div>)} 
+    return (
+       <table>
+         <thead>
+           <tr>
+             <th>Username</th>
+             <th>Phone number</th>
+           </tr>
+         </thead>
+         <tbody>
+           <tr>
+             <td>{user.name ? user.name: 'eror' }</td>
+             <td>{user.phone ? user.phone : 'eror' }</td>
+           </tr>
+         </tbody>
+       </table>
     );
   }
